724501719b198cf847993ccc15d4b78e07707702
Daniel <danielchochlisnki@hotmail.com>
2023-06-27 16:16:36 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import HomeView from "./views/home/HomeView";
 
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -4,12 +4,23 @@ interface TAppContext {
   children: ReactNode;
 }
 
-const AppContext = createContext({});
+interface AppContextValue {
+  setTestContext: (status: React.SetStateAction<number>) => void;
+  test: number;
+}
+
+const AppContext = createContext<AppContextValue>({
+  setTestContext: (_status: React.SetStateAction<number>) => {},
+  test: 1,
+});
 
 export const AppContextProvider = ({ children }: TAppContext) => {
-  
+  const [test, setTest] = useState<number>(0);
+  const setTestContext = (status: React.SetStateAction<number>) => {
+    setTest(status);
+  };
   return (
-    <AppContext.Provider value={{}}>
+    <AppContext.Provider value={{ setTestContext, test }}>
       <div>{children}</div>
     </AppContext.Provider>
   );

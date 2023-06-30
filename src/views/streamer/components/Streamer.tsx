033593d545c6e2ae9a0a6a4e6034ce66ee63c933
Daniel <danielchochlisnki@hotmail.com>
2023-06-30 17:38:17 -0400

    streamer initial

---
+++ src/views/streamer/components/Streamer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Streamer.module.scss";
+const Streamer = () => {
+  return (
+    <div>
+      <figure>
+        <img src="https://assets.codepen.io/605876/person.png" alt="" />
+      </figure>
+    </div>
+  );
+};
+
+export default Streamer;

b4b747f387df4ec7e6fbd6abde36ad2126c11a3e
Daniel <danielchochlisnki@hotmail.com>
2023-07-01 16:35:27 -0400

    commetns

--- src/views/home/components/MobileList.tsx
+++ src/views/home/components/MobileList.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-// import required modules
 import { Pagination, Navigation } from "swiper";
 import Card from "./Card";
 import { IStreamer } from "../../../types/types";

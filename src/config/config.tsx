16983fcde366828cb2c65e6be8b051ee0eb4c4cf
Daniel <danielchochlisnki@hotmail.com>
2023-07-01 22:36:22 -0400

    added extra env

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,4 +1,5 @@
 export const BASE_URL =
   process.env.REACT_APP_NODE_ENV === "dev"
-    ? "http://localhost:8081/api"
-    : "production url to be established";
+    ? process.env.REACT_APP_LOCAL_URL || "http://localhost:8081/api"
+    : process.env.REACT_APP_PROD_URL ||
+      "https://streamers-api.onrender.com/api";
